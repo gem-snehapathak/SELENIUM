8f65a3bc239cf13a7342a6743fe9eaf07324fbcd
Sneha Pathak <Sneha.Pathak@Geminisolutions.com>
2024-03-13 14:58:48 +0530

    changes done

--- src/main/java/org/example/BasicMethods.java
+++ src/main/java/org/example/BasicMethods.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
 
 public class BasicMethods {
     public static void main(String[] args) {
@@ -10,7 +14,7 @@ public class BasicMethods {
 
         ChromeDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com"); //1.get()
+        driver.get("https://www.saucedemo.com/v1/"); //1.get()
         driver.manage().window().maximize();  //2. manage()
 
         String Url = driver.getCurrentUrl();   //3.getCurrentUrl
@@ -22,9 +26,30 @@ public class BasicMethods {
         String pageSource= driver.getPageSource(); //5.getPageSource
         System.out.println(pageSource);
 
-        driver.navigate().to("https://www.amazon.in/");   //6.navigate()
-        driver.close();                             //7.close()
-                                                    //8.quit()
+         //       driver.navigate().to("https://www.amazon.in/");    //6.navigate()
+
+        //        driver.close();                                  //7.close()
+       //         driver.quit();                                   // 8.quit()
+
+        driver.findElement(By.xpath("//input[@name=\"user-name\"]")).sendKeys("standard_user"); //9.findElement
+        driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
+
+        List<WebElement> webElements = driver.findElements(By.xpath("//div[@class=\"inventory_list\"]/div")); //10.findElements
+        System.out.println(webElements);
+
+        driver.navigate().to("https://www.google.com");
+        String windowhandle = driver.getWindowHandle();    //11. getWindowHandle()
+        System.out.println(windowhandle);
+
+        driver.findElement(By.xpath("/html/body/div[1]/div[6]/div[2]/div[1]/a[2]")).click();
+        Set<String> windowhandle1 = driver.getWindowHandles(); //12.getWindowHandles()
+        System.out.println(windowhandle1);
+
+        driver.quit();
+
+
+
 
 
     }
