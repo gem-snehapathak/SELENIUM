1c5ef8337a04bc60a65e6297bdb8fa791dee95a9
Sneha Pathak <Sneha.Pathak@Geminisolutions.com>
2024-03-19 17:06:43 +0530

    Daily Updates

--- src/main/java/org/example/explicitWait.java
+++ src/main/java/org/example/explicitWait.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class explicitWait {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
